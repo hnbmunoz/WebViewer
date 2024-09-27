81eefd588a36030f9162abbae2f6aa79bd27e747
Henri Nicholli Muñoz <hnbmunoz@gmail.com>
2024-09-27 09:55:40 +0800

    Updated Readme and index file

--- src/components/modules/pdf/PDFSelector.tsx
+++ src/components/modules/pdf/PDFSelector.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback } from "react";
 import PDFViewer from "./PDFViewer";
 import ClearButton from "../../button/ClearButton";
 import { useDropzone } from "react-dropzone";
